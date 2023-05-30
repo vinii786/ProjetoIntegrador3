18a9accc7e8f56da46274663e8ed9d3fbf260d52
vinii786 <viniciussousa@unipam.edu.br>
2023-05-30 11:31:26 -0300

    create a infosHeadre

--- meu/src/App.js
+++ meu/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Slider from './components/slider/slider';
 import Header from './components/header/header';
-
+import InfosHeader from './components/infosheader/infosHeader';
 
 function App() {
   return (
     <div className="App">
       
       <Header />
-
+      <InfosHeader />
       <Slider />
 
     </div>
--- meu/src/components/infosheader/infosHeader.jsx
+++ meu/src/components/infosheader/infosHeader.jsx
@@ -1,3 +1,4 @@
+import './infosHeader.css'
 function infosHeader(){
     return(
         <div>
