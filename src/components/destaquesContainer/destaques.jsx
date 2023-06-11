41615fc6cbcba554b7dd68be5e92bc1345087315
vinii786 <viniciussousa@unipam.edu.br>
2023-06-11 19:42:54 -0300

    add home update

---
+++ src/components/destaquesContainer/destaques.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './containerBar.css'
+import BuyButton from '../buyButton/buyButton'
+import { Link } from "react-router-dom"
+function ContainerBar() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://projeto-integrador3.vercel.app/static/Destaques.json').then((Response) => Response.json())
+            .then(setData);
+    }, [])
+
+    return (
+        <div className="containerBar">
+            <div className="titles">
+                <h1>Destaques</h1>
+                <Link to={'/Destaques'}>
+                <p>Veja mais</p>
+                </Link>
+            </div>
+            <div className="teste">
+                {data.map((item) => {
+                    const { name, image, price } = item;
+                    return (
+                        <div className="prod_cards">
+                            <div className="prods">
+                                <img src={ image } alt="" />
+                                <h1>{ name }</h1>
+                                <p>{ price }</p>
+                                <BuyButton />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+        )
+}
+
+export default ContainerBar;
