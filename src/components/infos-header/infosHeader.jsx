c9517e48ff143483c89a641aacfa61739a65eec8
vinii786 <viniciussousa@unipam.edu.br>
2023-06-19 16:54:14 -0300

    update hora

--- src/components/infos-header/infosHeader.jsx
+++ src/components/infos-header/infosHeader.jsx
@@ -9,7 +9,7 @@ function InfosHeader() {
   var Hora = data.getHours()
   var res = <div className="time"></div>;
 
-  if (Hora > 0) {
+  if (Hora > 0 && Hora <= 7) {
     var time = 'fechado agora'
     res = (
       <div className="time" style={{ color: 'red' }}>
